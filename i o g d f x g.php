149279e7ef2d67fbc1a105f10cfaaf0c0471e456
Tingran Bian <btrzoe@163.com>
2019-05-01 10:26:16 +0100

    gbgbhgnbh

---
+++ i o g d f x g.php
@@ -0,0 +1,25 @@
+<?php
+header("content-type:text/html;charset=utf8");
+
+$servername = "mydb.ce12oaotat2e.eu-west-2.rds.amazonaws.com";
+$username = "root";
+$password = "12341234";
+
+// 创建连接
+$conn = mysqli_connect($servername, $username, $password,'testdb');
+
+// 检测连接
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql="SELECT * FROM staff";
+
+$result = mysqli_query($conn,$sql);
+while($row = mysqli_fetch_array($result))
+{
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "</tr>";
+}
